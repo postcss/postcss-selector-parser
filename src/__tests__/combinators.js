be4f3103d1279228535aa166501c967a98c54ca2
Ben Briggs <beneb.info@gmail.com>
2015-05-29 12:54:47 +0100

    Replaced duplicated test.

--- src/__tests__/combinators.js
+++ src/__tests__/combinators.js
@@ -9,18 +9,18 @@ test('column combinator', '.selected||td', (t, tree) => {
     t.equal(tree.selectors[0].rules[0].rules[0].value, 'td');
 });
 
+test('column combinator (2)', '.selected || td', (t, tree) => {
+    t.plan(3);
+    t.equal(tree.selectors[0].rules[0].value, 'selected');
+    t.equal(tree.selectors[0].rules[0].combinator, ' || ');
+    t.equal(tree.selectors[0].rules[0].rules[0].value, 'td');
+});
+
 test('descendant combinator', 'h1 h2', (t, tree) => {
     t.plan(3);
     t.equal(tree.selectors[0].rules[0].value, 'h1');
     t.equal(tree.selectors[0].rules[0].combinator, ' ');
     t.equal(tree.selectors[0].rules[0].rules[0].value, 'h2');
-});
-
-test('column combinator', '.selected||td', (t, tree) => {
-    t.plan(3);
-    t.equal(tree.selectors[0].rules[0].value, 'selected');
-    t.equal(tree.selectors[0].rules[0].combinator, '||');
-    t.equal(tree.selectors[0].rules[0].rules[0].value, 'td');
 });
 
 test('multiple descendant combinators', 'h1 h2 h3 h4', (t, tree) => {

ff2d538d985aa2f1194d954b393f7600fb12cc94
Adam Shelley <ashelley@retool.co>
2015-08-26 15:26:04 -0700

    fix error when parsing invalid pseudo class

--- src/__tests__/exceptions.js
+++ src/__tests__/exceptions.js
@@ -3,6 +3,7 @@
 import {throws} from './util/helpers';
 
 throws('unclosed pseudo element', 'button::');
+throws('unclosed pseudo class', 'a:');
 throws('bad pseudo element', 'button::"after"');
 throws('unclosed attribute selector', '[name="james"][href');
 throws('missing closing parenthesis in pseudo', ':not([attr="test"]:not([attr="test"])');

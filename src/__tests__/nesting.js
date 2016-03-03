5960680bec937b2a235835389c9a7e504a052823
Ben Briggs <beneb.info@gmail.com>
2016-03-03 13:06:10 +0000

    Add tests for @jeddy3.

--- src/__tests__/nesting.js
+++ src/__tests__/nesting.js
@@ -13,3 +13,24 @@ test('nesting selector followed by a class', '& .class', (t, tree) => {
     t.same(tree.nodes[0].nodes[2].value, 'class');
     t.same(tree.nodes[0].nodes[2].type, 'class');
 });
+
+test('&foo', '&foo', (t, tree) => {
+    t.same(tree.nodes[0].nodes[0].value, '&');
+    t.same(tree.nodes[0].nodes[0].type, 'nesting');
+    t.same(tree.nodes[0].nodes[1].value, 'foo');
+    t.same(tree.nodes[0].nodes[1].type, 'tag');
+});
+
+test('&-foo', '&-foo', (t, tree) => {
+    t.same(tree.nodes[0].nodes[0].value, '&');
+    t.same(tree.nodes[0].nodes[0].type, 'nesting');
+    t.same(tree.nodes[0].nodes[1].value, '-foo');
+    t.same(tree.nodes[0].nodes[1].type, 'tag');
+});
+
+test('&_foo', '&_foo', (t, tree) => {
+    t.same(tree.nodes[0].nodes[0].value, '&');
+    t.same(tree.nodes[0].nodes[0].type, 'nesting');
+    t.same(tree.nodes[0].nodes[1].value, '_foo');
+    t.same(tree.nodes[0].nodes[1].type, 'tag');
+});

f324d7e0490bcbe4ae2f4fcecb6a5d0471b4f93d
Ben Briggs <beneb.info@gmail.com>
2016-06-19 22:07:08 +0100

    Use arrow functions in the main entry point.

--- src/index.js
+++ src/index.js
@@ -12,57 +12,20 @@ import Str        from './selectors/string';
 import Tag        from './selectors/tag';
 import Universal  from './selectors/universal';
 
-let parser = function (processor) {
-    return new Processor(processor);
-};
+let parser = processor => new Processor(processor);
 
-parser.attribute = function (opts) {
-    return new Attribute(opts);
-};
-
-parser.className = function (opts) {
-    return new ClassName(opts);
-};
-
-parser.combinator = function (opts) {
-    return new Combinator(opts);
-};
-
-parser.comment = function (opts) {
-    return new Comment(opts);
-};
-
-parser.id = function (opts) {
-    return new Id(opts);
-};
-
-parser.nesting = function (opts) {
-    return new Nesting(opts);
-};
-
-parser.pseudo = function (opts) {
-    return new Pseudo(opts);
-};
-
-parser.root = function (opts) {
-    return new Root(opts);
-};
-
-parser.selector = function (opts) {
-    return new Selector(opts);
-};
-
-parser.string = function (opts) {
-    return new Str(opts);
-};
-
-parser.tag = function (opts) {
-    return new Tag(opts);
-};
-
-parser.universal = function (opts) {
-    return new Universal(opts);
-};
+parser.attribute = opts => new Attribute(opts);
+parser.className = opts => new ClassName(opts);
+parser.combinator = opts => new Combinator(opts);
+parser.comment = opts => new Comment(opts);
+parser.id = opts => new Id(opts);
+parser.nesting = opts => new Nesting(opts);
+parser.pseudo = opts => new Pseudo(opts);
+parser.root = opts => new Root(opts);
+parser.selector = opts => new Selector(opts);
+parser.string = opts => new Str(opts);
+parser.tag = opts => new Tag(opts);
+parser.universal = opts => new Universal(opts);
 
 export {
     TAG,

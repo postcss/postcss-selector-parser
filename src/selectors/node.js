a636e083e57d160bcf1ec0652ebb30e0bfaf0797
Ivan Kopeykin <ikopeykin@ozon.ru>
2019-08-29 14:34:42 +0300

    cast to string in stringifyProperty method

--- src/selectors/node.js
+++ src/selectors/node.js
@@ -148,7 +148,7 @@ export default class Node {
     }
 
     stringifyProperty (name) {
-        return (this.raws && this.raws[name]) || this[name];
+        return String((this.raws && this.raws[name]) || this[name]);
     }
 
     get rawSpaceBefore () {
@@ -178,7 +178,7 @@ export default class Node {
     }
 
     valueToString () {
-        return String(this.stringifyProperty("value"));
+        return this.stringifyProperty("value");
     }
 
     toString () {

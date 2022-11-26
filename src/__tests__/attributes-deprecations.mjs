9bb36f33ca1dbe2c93a64a23db9fc7ce1026e9e2
Romain Menke <romainmenke@gmail.com>
2022-11-26 17:14:02 +0100

    fix tests

--- src/__tests__/attributes-deprecations.mjs
+++ src/__tests__/attributes-deprecations.mjs
@@ -2,33 +2,42 @@ import attribute from '../selectors/attribute.js';
 import {test} from './util/helpers.mjs';
 const Attribute = attribute.default;
 
-// DeprecationWarnings are throw higher in the stack since Node 16.
-// These can no longer be caught.
-test.skip('deprecated constructor', '', (t) => {
-    t.throws(
-        () => {
-            return new Attribute({value: '"foo"', attribute: "data-bar"});
-        },
-        {message: "Constructing an Attribute selector with a value without specifying quoteMark is deprecated. Note: The value should be unescaped now."}
-    );
+function waitForWarning() {
+    return new Promise((resolve) => {
+        process.once('warning', (err) => {
+            resolve(err);
+        });
+    });
+}
+
+test.serial('deprecated constructor', '', (t) => {
+    const warningWaiter = waitForWarning();
+
+    new Attribute({ value: '"foo"', attribute: "data-bar" });
+
+    return warningWaiter.then((warning) => {
+        t.deepEqual(warning.message, "Constructing an Attribute selector with a value without specifying quoteMark is deprecated. Note: The value should be unescaped now.");
+    });
 });
 
-test.skip('deprecated get of raws.unquoted ', '', (t) => {
-    t.throws(
-        () => {
-            let attr = new Attribute({value: 'foo', quoteMark: '"', attribute: "data-bar"});
-            return attr.raws.unquoted;
-        },
-        {message: "attr.raws.unquoted is deprecated. Call attr.value instead."}
-    );
+test.serial('deprecated get of raws.unquoted ', '', (t) => {
+    const warningWaiter = waitForWarning();
+
+    let attr = new Attribute({ value: 'foo', quoteMark: '"', attribute: "data-bar" });
+    attr.raws.unquoted;
+
+    return warningWaiter.then((warning) => {
+        t.deepEqual(warning.message, "attr.raws.unquoted is deprecated. Call attr.value instead.");
+    });
 });
 
-test.skip('deprecated set of raws.unquoted ', '', (t) => {
-    t.throws(
-        () => {
-            let attr = new Attribute({value: 'foo', quoteMark: '"', attribute: "data-bar"});
-            attr.raws.unquoted = 'fooooo';
-        },
-        {message: "Setting attr.raws.unquoted is deprecated and has no effect. attr.value is unescaped by default now."}
-    );
+test.serial('deprecated set of raws.unquoted ', '', (t) => {
+    const warningWaiter = waitForWarning();
+
+    let attr = new Attribute({ value: 'foo', quoteMark: '"', attribute: "data-bar" });
+    attr.raws.unquoted = 'fooooo';
+
+    return warningWaiter.then((warning) => {
+        t.deepEqual(warning.message, "Setting attr.raws.unquoted is deprecated and has no effect. attr.value is unescaped by default now.");
+    });
 });

bbadc382b3327062e6454a69039948e153c4c772
btea <2356281422@qq.com>
2025-09-09 09:09:27 +0800

    perf: replace startsWith with strict equality

--- src/parser.js
+++ src/parser.js
@@ -581,7 +581,7 @@ export default class Parser {
             if (space.endsWith(' ') && rawSpace.endsWith(' ')) {
                 spaces.before = space.slice(0, space.length - 1);
                 raws.spaces.before = rawSpace.slice(0, rawSpace.length - 1);
-            } else if (space.startsWith(' ') && rawSpace.startsWith(' ')) {
+            } else if (space[0] === ' ' && rawSpace[0] === ' ') {
                 spaces.after = space.slice(1);
                 raws.spaces.after = rawSpace.slice(1);
             } else {

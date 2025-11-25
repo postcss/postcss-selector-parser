44ea4b12d51e5eda9ad9f096bcd8a79fc1926da6
Marco Pasqualetti <marco.pasqualetti@live.com>
2025-11-25 20:05:17 +0100

    fix(types): add `walkUniversal` declaration

--- postcss-selector-parser.test.ts
+++ postcss-selector-parser.test.ts
@@ -2,11 +2,18 @@ import * as parser from './postcss-selector-parser';
 
 parser((root) => {
     root.each((node, index) => {
-        node as parser.Selector;
-        index as number;
+        node satisfies parser.Selector;
+        index satisfies number;
     });
     root.walk((node, index) => {
-        node as parser.Selector;
-        index as number;
+        node satisfies parser.Node;
+        index satisfies number;
+
+        if (node.type === 'selector') {
+            node satisfies parser.Selector;
+        }
+    });
+    root.walkUniversals((node) => {
+        node satisfies parser.Universal;
     });
 }).processSync("a b > c");
